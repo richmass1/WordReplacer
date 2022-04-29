27bda082e28178ee1ff51f4ef25d28572ea7ef25
richmass1 <rmasserf@ucsd.edu>
2022-04-29 00:20:16 -0700

    Update WordReplacer.java

--- WordReplacer.java
+++ WordReplacer.java
@@ -1,6 +1,6 @@
 public class WordReplacer {
     public static String WordRep(String text, String target, String insert) {
-for(int i = text.indexOf(target); i != -1; i = text.indexOf(target, i + 1)) {
+for(int i = text.indexOf(target); i != -1; i = text.indexOf(target, i + insert.length() + 1)) {
 text = text.substring(0, i) + insert + text.substring(i + target.length());
 }
 return text;
